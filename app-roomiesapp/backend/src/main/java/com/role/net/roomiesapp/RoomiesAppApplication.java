5b1f4f0be962060d4a6e161a72581bd3c67c41f1
Antonio Henrique <henrique1508cops@gmail.com>
2026-07-02 21:24:59 -0300

    criando projeto base das instancias

---
+++ app-roomiesapp/backend/src/main/java/com/role/net/roomiesapp/RoomiesAppApplication.java
@@ -0,0 +1,13 @@
+package com.role.net.roomiesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomiesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomiesAppApplication.class, args);
+	}
+
+}
---
+++ app-tripmaker/backend/src/main/java/com/role/net/tripmaker/TripmakerApplication.java
@@ -0,0 +1,13 @@
+package com.role.net.tripmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmakerApplication.class, args);
+	}
+
+}
